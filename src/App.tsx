aab4a93c735b597868c86239dc5dd3480a21f095
vmarchaud <contact@vmarchaud.fr>
2021-04-03 12:29:08 +0200

    improv(build): use vite

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,19 @@ import { Provider } from "react-redux"
 import { ConnectedRouter } from "connected-react-router"
 import Routes from "./routes"
 import store, { history } from "./store"
-import { ConfigProvider } from "antd"
 import { IntlProvider } from "react-intl"
 import messages from "./shared/i18n"
-import moment from "moment"
-import frFR from "antd/lib/locale/fr_FR"
-import enGB from "antd/lib/locale/en_GB"
-import "moment/locale/fr"
-import "moment/locale/en-gb"
 
 const locale: "en" | "fr" = "en"
-// if (locale === "") {
-//   moment.locale("fr")
-// } else {
-//   moment.locale("en_GB")
-// }
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
-        <ConfigProvider locale={locale === "fr" ? frFR : enGB}>
-          <IntlProvider defaultLocale={locale} locale={locale} key={locale} messages={messages[locale]}>
-            <ConnectedRouter history={history}>
-              <Routes />
-            </ConnectedRouter>
-
-          </IntlProvider>
-        </ConfigProvider>
+      <IntlProvider defaultLocale={locale} locale={locale} key={locale} messages={messages[locale]}>
+        <ConnectedRouter history={history}>
+          <Routes />
+        </ConnectedRouter>
+      </IntlProvider>
     </Provider>
   )
 }
